7b3ca6d65ed5effd513b8471a74593007d0cc0b7
Huseyn Emin <huseynliemin68@gmail.com>
2024-08-19 23:03:25 +0100

    Apply table page has been created

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -5,11 +5,12 @@ export const Context = createContext();
 
 const DataProvider = ({ children }) => {
   const [data, setData] = useState([]);
+  const [dataApply, setDataApply] = useState([])
   const baseURL = import.meta.env.VITE_TENDERS;
   const tendersUrl = `${baseURL}/tenders`;
   const applyUrl = `${baseURL}/apply`;
 
- 
+
   const getTenders = async () => {
     try {
       const response = await axios.get(tendersUrl);
@@ -18,6 +19,15 @@ const DataProvider = ({ children }) => {
       console.error("Fetch data error", error);
     }
   };
+  const getApply = async () => {
+    try {
+      const response = await axios.get(applyUrl)
+      setDataApply(response.data ? response.data : [])
+
+    } catch (error) {
+      console.error("Fetch data error", error);
+    }
+  }
 
   const updateTender = async (id, updatedData) => {
     try {
@@ -38,13 +48,14 @@ const DataProvider = ({ children }) => {
       console.error("Delete error", error);
     }
   };
- 
+
   useEffect(() => {
     getTenders();
+    getApply()
   }, []);
 
   return (
-    <Context.Provider value={{ data, updateTender, deleteTender, setData, tendersUrl,applyUrl }}>
+    <Context.Provider value={{ data, updateTender, deleteTender, setData, tendersUrl, applyUrl, getApply, dataApply, setDataApply }}>
       {children}
     </Context.Provider>
   );
