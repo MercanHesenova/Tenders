9df9cf3ddb641dd8869362dce31c6217ae282144
Elza1199 <elza.rzayeva3@gmail.com>
2024-08-26 22:55:51 -0700

    adding icons and functionality to password input in singIn and signUp page and some css codes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,9 @@
 import { useFormik } from 'formik';
-import React ,{ useState, useEffect }from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
+import { IoMdEye, IoMdEyeOff } from "react-icons/io";
 import '../assets/signUp.css';
 
 
@@ -23,6 +24,8 @@ const SignupSchema = Yup.object().shape({
 const SignUp = () => {
   const navigate = useNavigate();
   const [signupData, setSignupData] = useState([]);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
 
   useEffect(() => {
@@ -49,14 +52,14 @@ const SignUp = () => {
     onSubmit: (values, actions) => {
 
       const existingUser = signupData.find(user =>
-        user.email === values.email || user.number === values.number
+        user.email == values.email || user.number == values.number
       );
 
       if (existingUser) {
-        if (existingUser.email === values.email) {
+        if (existingUser.email == values.email) {
           setErrors({ email: 'This email is already used' });
         }
-        if (existingUser.number === values.number) {
+        if (existingUser.number == values.number) {
           setErrors({ number: 'This phone number is already used' });
         }
         return;
@@ -74,61 +77,79 @@ const SignUp = () => {
     },
   });
 
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <>
-    <div className='body'>
-      <div className='signUpContainer'>
-        <div className='title'>Sign Up</div>
-        <form onSubmit={handleSubmit}>
-          <div className='user-details'>
-            <div className='input-wrapper'>
-              <label htmlFor="name" className='details'>Name</label>
-              <input type="text" id='name' placeholder='enter your name' value={values.name} onChange={handleChange} />
-              {touched.name && errors.name && <div className='error' style={{color: 'red'}}> {errors.name}</div>}
-            </div>
+      <div className='body'>
+        <div className='signUpContainer'>
+          <div className='title'>Sign Up</div>
+          <form onSubmit={handleSubmit}>
+            <div className='user-details'>
+              <div className='input-wrapper'>
+                <label htmlFor="name" className='details'>Name</label>
+                <input type="text" id='name' placeholder='enter your name' value={values.name} onChange={handleChange} />
+                {touched.name && errors.name && <div className='error' style={{ color: 'red' }}> {errors.name}</div>}
+              </div>
 
-            <div className='input-wrapper'>
-              <label htmlFor="surname" className='details'>Surname</label>
-              <input type="text" id='surname' placeholder='enter your surname' value={values.surname} onChange={handleChange} />
-              {touched.surname && errors.surname && <div className='error' style={{color: 'red'}}> {errors.surname}</div>}
-            </div>
+              <div className='input-wrapper'>
+                <label htmlFor="surname" className='details'>Surname</label>
+                <input type="text" id='surname' placeholder='enter your surname' value={values.surname} onChange={handleChange} />
+                {touched.surname && errors.surname && <div className='error' style={{ color: 'red' }}> {errors.surname}</div>}
+              </div>
 
-            <div className='input-wrapper'>
-              <label htmlFor="email" className='details'>Email</label>
-              <input type="email" id='email' placeholder='enter your email' value={values.email} onChange={handleChange} />
-              {touched.email && errors.email && <div className='error' style={{color: 'red'}}> {errors.email}</div>}
-            </div>
+              <div className='input-wrapper'>
+                <label htmlFor="email" className='details'>Email</label>
+                <input type="email" id='email' placeholder='enter your email' value={values.email} onChange={handleChange} />
+                {touched.email && errors.email && <div className='error' style={{ color: 'red' }}> {errors.email}</div>}
+              </div>
 
-            <div className='input-wrapper'>
-              <label htmlFor="number" className='details'>Phone Number</label>
-              <input type="text" id='number' placeholder='+99455 555 55 55' value={values.number} onChange={handleChange} />
-              {touched.number && errors.number && <div className='error' style={{color: 'red'}}> {errors.number}</div>}
-            </div>
+              <div className='input-wrapper'>
+                <label htmlFor="number" className='details'>Phone Number</label>
+                <input type="text" id='number' placeholder='+99455 555 55 55' value={values.number} onChange={handleChange} />
+                {touched.number && errors.number && <div className='error' style={{ color: 'red' }}> {errors.number}</div>}
+              </div>
 
-            <div className='input-wrapper'>
-              <label htmlFor="password" className='details'>Password</label>
-              <input type="password" id='password' placeholder='enter your password' value={values.password} onChange={handleChange} />
-              {touched.password && errors.password && <div className='error'style={{color: 'red'}}> {errors.password}</div>}
-            </div>
+              <div className='input-wrapper'>
+                <label htmlFor="password" className='details'>Password</label>
+                <div className='input-box'>
+                  <input type={showPassword ? 'text' : 'password'} id='password' placeholder='enter your password' value={values.password} onChange={handleChange} />
+                  <button type="button" onClick={togglePasswordVisibility} className='iconEye'>
+                    {showPassword ? <IoMdEye /> : <IoMdEyeOff />}
+                  </button>
+                </div>
+                {touched.password && errors.password && <div className='error' style={{ color: 'red' }}> {errors.password}</div>}
+              </div>
 
-            <div className='input-wrapper'>
-              <label htmlFor="confirmPassword" className='details'>Confirm Password</label>
-              <input type="password" id='confirmPassword' placeholder='confirm your password' value={values.confirmPassword} onChange={handleChange} />
-              {touched.confirmPassword && errors.confirmPassword && <div className='error'style={{color: 'red'}}> {errors.confirmPassword}</div>}
-            </div>
-            
-            <div className='button'>
-              <button type='submit'>Create an account</button>
-            </div>
+              <div className='input-wrapper'>
+                <label htmlFor="confirmPassword" className='details'>Confirm Password</label>
+                <div className='input-box'>
+                  <input type={showConfirmPassword ? 'text' : 'password'} id='confirmPassword' placeholder='confirm your password' value={values.confirmPassword} onChange={handleChange} />
+                  <button type="button" onClick={toggleConfirmPasswordVisibility} className='iconEye'>
+                    {showConfirmPassword ? <IoMdEye /> : <IoMdEyeOff />}
+                  </button>
+                </div>
+                {touched.confirmPassword && errors.confirmPassword && <div className='error' style={{ color: 'red' }}> {errors.confirmPassword}</div>}
+              </div>
 
-           
-          </div>
-          <div className='login-link'>
-            <p>Already have an account?<Link to="/sign-in"> Login</Link></p>
-            </div>
-        </form>
+              <div className='button'>
+                <button type='submit'>Create an account</button>
+              </div>
 
-      </div>
+
+            </div>
+            <div className='login-link'>
+              <p>Already have an account?<Link to="/sign-in"> Login</Link></p>
+            </div>
+          </form>
+
+        </div>
       </div>
     </>
   )
